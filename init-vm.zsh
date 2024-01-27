36d813e9b0ff2b836a23dbd0d1fd9cb193c26057
Pablo de Andrade Silva <pablodeas@gmail.com>
2024-01-27 19:41:20 -0300

    version:0.2

--- init-vm.zsh
+++ init-vm.zsh
@@ -1,31 +1,68 @@
 #!/bin/zsh
 
 # VM's
-vm1="regolith_i3"
+vm1="opensuse01"
 
 clear
 
-echo ""
-echo "-> VM's disponíveis:"
-echo " 1. $vm1"
-sleep 1
+function logo () {
+  echo ""
+  echo "---------------- -------------------------------- --------------"
+  echo "---------------- Gerenciador de Máquinas Virtuais --------------" 
+  echo "---------------- -------------------------------- --------------"
+}
 
-echo ""
-echo "-> Digite o número correspondente a VM que deseja ligar:"
-read num
+# Precisa transformar em uma função 'func menu'
+function menu () {
+  echo ""
+  echo "---"
+  echo "-> VM's disponíveis:"
+  echo " 1. $vm1"
+  echo "---"
+  sleep 1
 
+  echo ""
+  echo "---"
+  echo "-> Digite o número correspondente a VM que deseja ligar:"
+  echo "-> Caso deseje sair, digite 0:"
+  echo "---"
+  read num
+}
+# Até aqui deve ser a função 'func menu'
 
-case $num in
-  1)
-    echo ""
-    echo "-> VM $vm1 iniciando."
-    echo ""
-    virsh start $vm1
-    ;;
-  *)
-    echo ""
-    echo "-> Opção Inválida."
-    echo ""
-    ;;
-esac
+# Aqui vai ser a função para verificar as ações
+function option_verify () {
+  case $num in
+    0)
+      echo ""
+      echo "---"
+      echo "-> Saindo..."
+      echo "---"
+      echo ""
+      exit
+      ;;
+    1)
+      echo ""
+      echo "---"
+      echo "-> VM $vm1 iniciando."
+      echo "---"
+      echo ""
+      virsh start $vm1
+      ;;
+    *)
+      echo ""
+      echo "---"
+      echo "-> Opção Inválida. Tente novamente"
+      echo "---"
+      echo ""
+      menu
+      option_verify
+      ;;
+  esac
+}
 
+logo
+menu
+option_verify
+# Executar o menu e depois as opções
+# Nas opções, caso a opção selecionada seja inválida, deve se retornar para o menu e captar novamente a ação do usuário
